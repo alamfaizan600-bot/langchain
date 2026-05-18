76f4784e1ec267c3cfad7553627be2b10058f375
Faizan <faizan@example.com>
2026-05-18 07:04:24 +0000

    basic agentcodes

---
+++ chain_agent_1.py
@@ -0,0 +1,29 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain.agents import create_agent
+from langchain.tools import tool
+from langchain_core.messages import HumanMessage
+
+from tavily import TavilyClient
+
+tavily = TavilyClient()  # Initialize the Tavily client
+llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash", temperature=0.1)
+
+@tool
+def search(query: str) -> str:
+    """
+    Arguments:
+    query: The search query to be executed.
+    Returns:
+    A string representing the search results.
+    """
+    print(f"Executing search for query: {query}")
+    return tavily.search(query=query)
+
+tools = [search]   
+agent = create_agent(model=llm, tools=tools) 
+
+output = agent.invoke({"messages": [HumanMessage(content="What is the weather like in Delhi, India?")]})
+print(output.content)
